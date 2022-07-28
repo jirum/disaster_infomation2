8ebedcaa6c4812cce4e39202916bcf38df408e90
jirum <monteclar.jerome19@gmail.com>
2022-07-28 14:38:52 +0800

    add function edit for category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :check_admin
+  before_action :set_category, only: [:edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -19,7 +20,21 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def check_admin
     if current_user.client?
