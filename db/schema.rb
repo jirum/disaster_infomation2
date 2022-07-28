86c66cf8ca9b35c09178c8f6634eb90e2639f787
jirum <monteclar.jerome19@gmail.com>
2022-07-28 12:23:06 +0800

    migrate posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_035822) do
+ActiveRecord::Schema.define(version: 2022_07_28_040746) do
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "address"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
