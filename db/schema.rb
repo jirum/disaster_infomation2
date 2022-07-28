5a3c8677fb0e6a39ef0eaa87529f86d9060daea4
jirum <monteclar.jerome19@gmail.com>
2022-07-28 14:16:38 +0800

    migrate category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_055751) do
+ActiveRecord::Schema.define(version: 2022_07_28_060812) do
+
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
