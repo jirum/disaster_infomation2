2c611a31712ca199bea5b563d776c84b377c131c
jirum <monteclar.jerome19@gmail.com>
2022-07-28 17:29:14 +0800

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Typhoon')
+Category.create(name: 'Earthquake')
+Category.create(name: 'Flood')
+Category.create(name: 'Covid')
+
