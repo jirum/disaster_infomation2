341632ee978b004908b661f85c2b1b59fa55ddbd
jirum <monteclar.jerome19@gmail.com>
2022-07-28 14:45:21 +0800

    add column for category id for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_060812) do
+ActiveRecord::Schema.define(version: 2022_07_28_064228) do
 
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_060812) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
