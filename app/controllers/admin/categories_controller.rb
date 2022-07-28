904c70488e722856c47396e644c731e210244e2a
jirum <monteclar.jerome19@gmail.com>
2022-07-28 14:36:04 +0800

    add function to create new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,11 +6,28 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def check_admin
     if current_user.client?
       redirect_to posts_path
     end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
