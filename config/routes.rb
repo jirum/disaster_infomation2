ea1fa811160cecd31fd392d3b3d5892127dd071a
jirum <monteclar.jerome19@gmail.com>
2022-07-29 13:52:30 +0800

    add routes for current user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => "posts#index"
+  get '/current_user/posts' => "posts#current_user_post"
 
   resources :posts do
     resources :comments
